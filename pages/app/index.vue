bf0fcd3cee001069babc80dfc19887d8150f01c2
Piotr Wrobel <piotr.wrobel@hotmail.com>
2021-02-26 23:35:06 +0100

    heading styling

--- pages/app/index.vue
+++ pages/app/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col items-center justify-center p-8">
-    <Logo />
+    <h1>Strona Główna</h1>
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam repellendus totam obcaecati, non ex sint molestiae numquam distinctio, dignissimos odit eaque cumque voluptates facere esse? Cupiditate eaque laborum alias repellendus?
     </p>
--- pages/app/search.vue
+++ pages/app/search.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col items-center justify-center p-8">
-    <h1>Szukajka</h1>
+    <h1>Wyszukiwarka</h1>
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam repellendus totam obcaecati, non ex sint molestiae numquam distinctio, dignissimos odit eaque cumque voluptates facere esse? Cupiditate eaque laborum alias repellendus?
     </p>
