b0fcf5327057484571e8daebdb93236c727f5617
Piotr Wrobel <piotr.wrobel@hotmail.com>
2021-02-28 04:28:52 +0100

    fix navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,27 +1,31 @@
 <template>
   <div class="fixed flex justify-around items-center bottom-0 w-screen h-16 bg-white dark:bg-gray-700 shadow-lg border-t border-gray-200 dark:border-gray-600">
     
-    <div class="w-1/4">
+    <div >
       <NuxtLink class="flex flex-col items-center text-gray-400" to="/app">
-        <solid-home-icon class="w-7 h-7" />
+        <solid-fire-icon class="w-7 h-7" />
+        <span class="text-xs font-bold">Główna</span>
       </NuxtLink>    
     </div>
 
-    <div class="w-1/4">
-      <NuxtLink :class="{'nuxt-link-exact-active': isRouteActive('/app/database') }" class="flex flex-col items-center text-gray-400" to="/app/database">
-        <solid-film-icon class="w-7 h-7" />
-      </NuxtLink>    
-    </div>
-
-    <div class="w-1/4">
+    <div>
       <NuxtLink :class="{'nuxt-link-exact-active': isRouteActive('/app/search') }" class="flex flex-col items-center text-gray-400" to="/app/search">
         <solid-search-icon class="w-7 h-7" />
+        <span class="text-xs font-bold">Szukaj</span>
       </NuxtLink>    
     </div>
 
-    <div class="w-1/4">
+    <div>
       <NuxtLink :class="{'nuxt-link-exact-active': isRouteActive('/app/favorites') }" class="flex flex-col items-center text-gray-400" to="/app/favorites">
-        <solid-heart-icon class="w-7 h-7" />
+        <outline-clock-icon class="w-7 h-7" />
+        <span class="text-xs font-bold">Playlista</span>
+      </NuxtLink>    
+    </div>
+
+    <div>
+      <NuxtLink :class="{'nuxt-link-exact-active': isRouteActive('/app/database') }" class="flex flex-col items-center text-gray-400" to="/app/database">
+        <solid-cog-icon class="w-7 h-7" />
+        <span class="text-xs font-bold">Ustawienia</span>
       </NuxtLink>    
     </div>
   </div>
