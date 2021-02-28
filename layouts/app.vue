b2b18adfd87e1383a5c97a6caa4498a2a273236c
Piotr Wrobel <piotr.wrobel@hotmail.com>
2021-02-28 16:02:19 +0100

    hot fix

--- layouts/app.vue
+++ layouts/app.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-searchflix z-10 bg-cover relative min-h-screen">
-    <div class="absolute z-20 inset-0 bg-black bg-opacity-80"></div>
+    <div class="absolute z-20 inset-0 bg-black bg-opacity-90"></div>
     <div class="absolute inset-0 z-30">
       <Nuxt />
       <Navbar />         
