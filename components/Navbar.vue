b1e864798097977985ed6aa51e04cf7c48e60f30
Piotr Wrobel <piotr.wrobel@hotmail.com>
2021-03-03 23:09:22 +0100

    change navbar color

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,29 +1,29 @@
 <template>
-  <div class="fixed flex justify-around items-center bottom-0 w-screen h-16 bg-white dark:bg-gray-700 shadow-lg border-t border-gray-200 dark:border-gray-600">
+  <div class="fixed flex justify-around items-center bottom-0 w-screen h-16 bg-black dark:bg-gray-700 shadow-lg border-t border-gray-900 dark:border-gray-600">
     
     <div >
-      <NuxtLink class="flex flex-col items-center text-gray-400" to="/app">
+      <NuxtLink class="flex flex-col items-center text-gray-300" to="/app">
         <solid-fire-icon class="w-7 h-7" />
         <span class="text-xs font-bold">Główna</span>
       </NuxtLink>    
     </div>
 
     <div>
-      <NuxtLink :class="{'nuxt-link-exact-active': isRouteActive('/app/search') }" class="flex flex-col items-center text-gray-400" to="/app/search">
+      <NuxtLink :class="{'nuxt-link-exact-active': isRouteActive('/app/search') }" class="flex flex-col items-center text-gray-300" to="/app/search">
         <solid-search-icon class="w-7 h-7" />
         <span class="text-xs font-bold">Szukaj</span>
       </NuxtLink>    
     </div>
 
     <div>
-      <NuxtLink :class="{'nuxt-link-exact-active': isRouteActive('/app/favorites') }" class="flex flex-col items-center text-gray-400" to="/app/favorites">
+      <NuxtLink :class="{'nuxt-link-exact-active': isRouteActive('/app/favorites') }" class="flex flex-col items-center text-gray-300" to="/app/favorites">
         <outline-clock-icon class="w-7 h-7" />
         <span class="text-xs font-bold">Playlista</span>
       </NuxtLink>    
     </div>
 
     <div>
-      <NuxtLink :class="{'nuxt-link-exact-active': isRouteActive('/app/database') }" class="flex flex-col items-center text-gray-400" to="/app/database">
+      <NuxtLink :class="{'nuxt-link-exact-active': isRouteActive('/app/database') }" class="flex flex-col items-center text-gray-300" to="/app/database">
         <solid-cog-icon class="w-7 h-7" />
         <span class="text-xs font-bold">Ustawienia</span>
       </NuxtLink>    
@@ -52,5 +52,9 @@ export default {
 
   a.nuxt-link-exact-active {
     @apply text-red !important;
+
+    & span {
+      @apply text-gray-300;
+    }
   }
 </style>
