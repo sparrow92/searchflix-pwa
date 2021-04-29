8c50b0ae0926d05fdc35c27369a935f5783a2549
Piotr Wrobel <piotr.wrobel@hotmail.com>
2021-04-29 05:35:55 +0200

    posters in favorites styling

--- pages/app/favorites.vue
+++ pages/app/favorites.vue
@@ -1,9 +1,17 @@
 <template>
   <div>
     <h1 class="mx-8">Do obejrzenia</h1>
-    <p class="mx-8">
-      <img v-for="movie in getMovies" :key="movie.id" :src="movie.image" @click="open(movie.id)">  
-    </p>
+    <div class="grid mx-8 gap-4">
+      <div 
+        @click="open(movie.id)" 
+        v-for="movie in getMovies" 
+        :key="movie.id" 
+        :style="[bgStyle, { backgroundImage: `url(${movie.image})` }]" 
+        class="relative flex justify-between items-center"
+      >
+        <solid-star-icon class="w-7 h-7 text-red shadow-2xl cursor-pointer absolute right-2 top-2 m-auto" @click="removeMovie(movie.id)"  />
+      </div>
+    </div>
     <Details :id="id" :show="showDetails" @close="close" /> 
   </div>
 </template>
@@ -24,10 +32,24 @@ export default {
   computed: {
     ...mapGetters([ 
       'getMovies'
-    ])
+    ]),
+
+    bgStyle() {
+      return {
+        width: '120px',
+        paddingTop: '142%',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center center'
+      }
+    }
   },
 
   methods: {
+    ...mapActions([
+      'removeMovie'
+    ]),
+
     close: function() {
       this.showDetails = false;
     },
