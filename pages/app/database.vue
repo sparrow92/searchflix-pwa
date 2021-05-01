dcbe05323fd632d243bafaba2e47cb4db4ad3313
Piotr Wrobel <piotr.wrobel@hotmail.com>
2021-05-01 22:12:40 +0200

    remove paragraph from settings

--- pages/app/database.vue
+++ pages/app/database.vue
@@ -8,9 +8,6 @@
     </p>
     
     <Blacklist :show="showBlacklist" @close="close" />
-    <p class="mx-8">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam repellendus totam obcaecati, non ex sint molestiae numquam distinctio, dignissimos odit eaque cumque voluptates facere esse? Cupiditate eaque laborum alias repellendus?
-    </p>
   </div>
 </template>
 
