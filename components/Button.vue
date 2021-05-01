d42cf47ba28c0aea23f2ff1f2493074174cf261a
Piotr Wrobel <piotr.wrobel@hotmail.com>
2021-05-01 21:40:23 +0200

    remove margins from button

--- components/Button.vue
+++ components/Button.vue
@@ -1,6 +1,6 @@
 <template>
     <button 
-      class="mt-5 rounded uppercase bg-red font-bold shadow-lg" 
+      class="rounded uppercase bg-red font-bold shadow-lg" 
       :class="small ? 'text-sm py-2 px-5' : 'text-lg py-3 px-8'"
     >
       <slot />
