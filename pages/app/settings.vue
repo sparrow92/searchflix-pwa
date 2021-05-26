27e7f5f031a331b93c0552322e4bf46b7379326d
Piotr Wrobel <piotr.wrobel@hotmail.com>
2021-05-26 09:50:29 +0200

    add info box

--- pages/app/settings.vue
+++ pages/app/settings.vue
@@ -2,10 +2,19 @@
   <div>
     <h1 class="mx-8">Ustawienia</h1>
     <SelectCountry />
-    <p class="p-8">
+    <div class="p-8">
+      <h1 class="text-xl font-bold">Czarna lista</h1>
+      <div class="flex items-center gap-3 p-3 bg-opacity-50 bg-gray-700 rounded text-gray-400 font-semibold mb-3">
+        <div class="flex-grow">
+          <solid-information-circle-icon class="w-5 h-5" />
+        </div>
+        <p class="block">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, id harum corrupti itaque perferendis repudiandae, nisi numquam tempora quo, illo eligendi suscipit recusandae facere nostrum eius. Minus nostrum animi incidunt!</p>        
+      </div>
+
+
       <Button @click.native="open">Blacklist</Button>
       <span class="block" v-for="item in getBlacklist" :key="item.netflixid" @click="removeGenre(item.netflixid)">{{ item.genre }}</span>
-    </p>
+    </div>
     
     <Blacklist :show="showBlacklist" @close="close" />
   </div>
