1dd3cd117e58aecc703e064c4dbdb0310b22a0fc
Piotr Wrobel <piotr.wrobel@hotmail.com>
2021-05-29 03:28:48 +0200

    make heading border optional

--- components/Heading.vue
+++ components/Heading.vue
@@ -1,12 +1,12 @@
 <template>
   <div :class="big ? 'w-screen md:bg-gradient-to-r from-red-900 via-red-500 to-red-900 shadow-inner mb-5' : 'w-full'">
     <div class="flex justify-between items-center py-2" :class="[{ 'px-8 py-4 h-20': big }]">
-      <h1 :class="['mb-0', { 'text-xl font-bold': !big }]">{{ title }}</h1>
+      <h1 :class="['mb-0', { 'text-2xl font-semibold uppercase py-2': !big }]">{{ title }}</h1>
       <div class="heading__additional">
         <slot />      
       </div>
     </div>
-    <div v-if="!big" class="bg-gradient-to-r from-gray-500 via-gray-600 to-transparent h-px mb-5"></div> 
+    <div v-if="!big && border" class="bg-gradient-to-r from-gray-700 via-gray-700 to-transparent h-px mb-8"></div> 
   </div>
 
 </template>
@@ -22,6 +22,11 @@ export default {
     title: {
       type: String,
       default: () => ''
+    },
+
+    border: {
+      type: Boolean,
+      default: () => true
     }
   },
 
