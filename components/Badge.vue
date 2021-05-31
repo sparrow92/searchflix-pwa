bf5dec712a5113b5b0a720324fbb5fede97a9f50
Piotr Wróbel <p.wrobel@clickmaster.pl>
2021-05-31 13:00:52 +0200

    create badge component

---
+++ components/Badge.vue
@@ -0,0 +1,34 @@
+<template>
+  <span class="flex items-center gap-2 rounded-full py-2 px-5 mx-0.5 my-1 text-sm font-semibold bg-white text-gray-800 transition-all duration-200 bg-opacity-80 hover:bg-opacity-100 xs:whitespace-nowrap cursor-pointer">
+    <slot />
+    <solid-x-icon @click.native="remove" class="w-3 h-3 cursor-pointer" />
+  </span>    
+</template>
+
+<script>
+export default {
+  props: {
+
+  },
+
+  data() {
+    return {
+      backgroundImage: ''
+    }
+  },
+
+  computed: {
+
+  },
+
+  methods: {
+    remove: function () {
+      this.$emit('remove');
+    },
+  }
+}
+</script>
+
+<style lang="postcss" scoped>
+
+</style>
--- pages/app/settings.vue
+++ pages/app/settings.vue
@@ -10,10 +10,7 @@
       </Heading>
             
       <div class="flex flex-wrap">
-        <span class="flex items-center gap-2 rounded-full py-2 px-5 mx-0.5 my-1 text-sm font-semibold bg-white text-gray-800 transition-all duration-200 bg-opacity-80 hover:bg-opacity-100 xs:whitespace-nowrap cursor-pointer" v-for="item in getBlacklist" :key="item.netflixid">
-          {{ item.genre }}
-          <solid-x-icon @click.native="removeGenre(item.netflixid)" class="w-3 h-3 cursor-pointer" />
-        </span>        
+        <Badge v-for="item in getBlacklist" :key="item.netflixid" @remove="removeGenre(item.netflixid)">{{ item.genre }}</Badge>
       </div>
 
     </div>
